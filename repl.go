e2bb7dee7920627a471dbe2670f4537e285f7e29
ferneediaz <dandiaz10@yahoo.com>
2024-09-19 21:57:12 +0800

    testing git

--- repl.go
+++ repl.go
@@ -6,12 +6,15 @@ import (
     "os"
 )
 
-func main() {
-    scanner := bufio.NewScanner(os.Stdin) // corrected assignment
-    fmt.Println("PLEASE ENTER SOME TEXT")
+func startRepl () {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Println("PLEASE ENTER SOME TEXT")
 
     scanner.Scan()
     text := scanner.Text()
 
     fmt.Println("echoing: ", text)
+	} 
+    
 }
